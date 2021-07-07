3b16e01d67cd63e56c797e01670101c057bf5693
DavidUrrego <urregodavid97@gmail.com>
2021-07-07 10:26:45 -0500

    Se agregan complementa las clases del dominio y se agrega La clase de interaccion con la base de datos

---
+++ src/main/java/datos/JugadorDAO.java
@@ -0,0 +1,161 @@
+
+package datos;
+
+import java.sql.*;
+import static datos.Conexcion.*;
+import domain.Jugador;
+
+public class JugadorDAO {
+
+    private static final String SQL_SELECT = "SELECT id, nombre, numero_victorias, pass FROM carros_consola.ganadores";   
+    private static final String SQL_INSERT = "INSERT INTO carros_consola.ganadores (nombre, numero_victorias,pass) VALUES(?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE carros_consola.ganadores SET   numero_victorias=?  WHERE nombre=?";
+    private static final String SQL_DELETE = "DELETE FROM carros_consola.ganadores WHERE nombre=?";
+
+    public JugadorDAO(){
+        
+    }
+    
+    public void desicion(Jugador jugador){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        boolean bandera=false ;
+        int numero_victorias=0;
+        try {
+            conn = Conexcion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                int pass = rs.getInt("pass");
+                if(jugador.getNombreJugador().equals(nombre) && jugador.getIdJgador()== pass){
+                    bandera=true;
+                    numero_victorias = rs.getInt("numero_victorias");                   
+                }                
+            }
+            if(bandera == true){
+                actualizar(jugador, numero_victorias);               
+            }
+            else{
+                insertar(jugador);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                Conexcion.close(rs);
+                Conexcion.close(stmt);
+                Conexcion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }    
+    }
+
+    public void seleccionar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn = Conexcion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.println("ID, Nombre, numero de victorias");
+                System.out.print(rs.getInt("id")+"  "); 
+                System.out.print(rs.getString("nombre")+"  ");
+                System.out.println(rs.getInt("numero_victorias"));                
+                
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                Conexcion.close(rs);
+                Conexcion.close(stmt);
+                Conexcion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+    }
+
+    public int insertar(Jugador jugador) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+        try {
+            conn = Conexcion.getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            stmt.setString(1, jugador.getNombreJugador());            
+            stmt.setInt(2, 1);
+            stmt.setInt(3, jugador.getIdJgador());            
+            registros = stmt.executeUpdate(); //PARA EJECUTAR SENTENCIAS DE DE ACTUALIZACIONDE BASE DE DATOS
+            //ESTE METODO PUEDE EJECUTAR SENTENCIAS DE INSERT UPDATE O DELETE
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                Conexcion.close(stmt);
+                Conexcion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        return registros;
+    }
+
+    public int actualizar(Jugador jugador,int n_victo) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+        try {
+            conn = Conexcion.getConnection();
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setInt(1, n_victo+1);
+            stmt.setString(2, jugador.getNombreJugador());
+            registros = stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                Conexcion.close(stmt);
+                Conexcion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+
+        return registros;
+    }
+    
+        public int eliminar(Jugador jugador) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+        try {
+            conn = Conexcion.getConnection();
+            stmt = conn.prepareStatement(SQL_DELETE);
+            stmt.setString(1, jugador.getNombreJugador());
+            registros = stmt.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                Conexcion.close(stmt);
+                Conexcion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+
+        return registros;
+    }
+}
+
+    
+
---
+++ src/main/java/domain/Podio.java
@@ -0,0 +1,29 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Podio {
+    private final List<Jugador> puestos;
+    
+    public Podio(){
+        this.puestos = new ArrayList<>();
+    }
+
+    
+    public void ingresar(Jugador puesto){
+        this.puestos.add(puesto);
+    }
+
+    public List <Jugador> getPuestos() {
+        return puestos;
+    }
+
+    
+    public void imprimirPodio(){
+        for (int i = 0; i < puestos.size(); i++) {
+            System.out.println("Lugar #"+i+" : "+ puestos.get(i).getNombreJugador());    
+            
+        }
+    }       
+}
